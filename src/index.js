1565cc204377811299f7bb803b7384c590d71ef9
Maki Giuseppe de la Vega Silva <pankthera@gmail.com>
2025-08-10 08:19:17 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    
+    // Reemplaza con la URL de tu proyecto de Astro en Pages
+    const targetUrl = 'https://podo-trenner.pankthera.workers.dev';
+    
+    const newUrl = targetUrl + url.pathname + url.search;
+    
+    const response = await fetch(newUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body
+    });
+    
+    return response;
+  }
+}
